b17dcda6d037e099bd45afd7ed997fce1998910c
Mukesh Kumar <mukesh.kumar1@myntra.com>
2016-10-23 15:29:08 +0530

    basic error handling via wrapped values

--- repl.c
+++ repl.c
@@ -36,8 +36,64 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
-long eval(mpc_ast_t*);
-long eval_op(long, char*, long);
+// Create Enumeration of Possible val Types
+enum { LVAL_NUM, LVAL_ERR };
+
+// Create Enumeration of Possible Error Types
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// lval struct to denote lisp return value enabling return of error or number
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+// number type for lval
+lval lval_num(long x){
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+//error type for lval
+lval lval_err(int x){
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+// print lval
+void lval_print(lval v){
+  switch (v.type){
+    // in case its a number print it
+    case LVAL_NUM: printf("%li", v.num); break;
+    // in case of error 
+    case LVAL_ERR:
+      switch(v.err){
+        case LERR_DIV_ZERO:
+          printf("Error: Division By Zero!");
+          break;
+        case LERR_BAD_OP:
+          printf("Error: Invalid Operator!");
+          break;
+        case LERR_BAD_NUM:
+          printf("Error: Invalid Number!");
+          break;
+        default:
+          printf("Unknown error, code:%i",v.err);
+      }
+      break;
+  }
+}
+
+// print lval followed by newline
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+lval eval(mpc_ast_t*);
+lval eval_op(lval, char*, lval);
 
 int main(int i, char** a){
   // polish notation parsers
@@ -71,8 +127,8 @@ int main(int i, char** a){
       // mpc_ast_print(r.output);
 
       // calculate result
-      long result = eval(r.output);
-      printf("%li\n", result);
+      lval result = eval(r.output);
+      lval_println(result);
       mpc_ast_delete(r.output);
     }
     else{
@@ -88,17 +144,21 @@ int main(int i, char** a){
   return 0;
 }
 
-long eval(mpc_ast_t* t){
+lval eval(mpc_ast_t* t){
   // if number return directly
   if (strstr(t->tag,"number")) {
-    return atoi(t->contents);
+    /* Check if there is some error in conversion */
+    errno = 0;
+    long x = strtol(t -> contents, NULL, 10);
+    // overflow situation
+    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
   }
 
   // else it is an expression
   // the operator is always second child
   char* op = t -> children[1] -> contents;
   // evaluate third child, base for calculations
-  long x = eval(t -> children[2]);
+  lval x = eval(t -> children[2]);
   // iterate over other children and combine
   int i = 3;
   while (strstr(t -> children[i] -> tag,"expr")) {
@@ -109,10 +169,17 @@ long eval(mpc_ast_t* t){
   return x;
 }
 
-long eval_op(long x, char* op, long y){
-  if(strcmp(op, "+") == 0){ return x + y; }
-  if(strcmp(op, "-") == 0){ return x - y; }
-  if(strcmp(op, "/") == 0){ return x / y; }
-  if(strcmp(op, "*") == 0){ return x * y; }
-  return 0;
+lval eval_op(lval x, char* op, lval y){
+    /* If either value is an error return it */
+  if (x.type == LVAL_ERR) { return x; }
+  if (y.type == LVAL_ERR) { return y; }
+
+  // otherwise do math
+  if(strcmp(op, "+") == 0){ return lval_num(x.num + y.num); }
+  if(strcmp(op, "-") == 0){ return lval_num(x.num - y.num); }
+  if(strcmp(op, "*") == 0){ return lval_num(x.num * y.num); }
+  if(strcmp(op, "/") == 0){
+    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+  }
+  return lval_err(LERR_BAD_OP);
 }
